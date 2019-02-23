43a797a2c78f11a4f8df353fba7ffaadb7a14ecf
Henry Chang <hchang1994@hotmail.com>
2019-02-23 13:16:43 -0800

    first commit

---
+++ public/js/canvas.js
@@ -0,0 +1,25 @@
+var cnv;
+
+function centerCanvas() {
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+}
+
+function setup() {
+  cnv = createCanvas(800, 500);
+  centerCanvas();
+  background(220);
+}
+
+function draw() {
+  if (mouseIsPressed) {
+    fill(0);
+    ellipse(mouseX, mouseY, 5, 5);
+  }
+}
+
+
+function windowResized() {
+  centerCanvas();
+}
